c515578d91615d88777dc539242cc14ea7bc838c
Niro Karunakaran <niro@tailit.com>
2021-07-24 18:06:14 +0200

    search results to excel file

--- webscrape.py
+++ webscrape.py
@@ -1,24 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import pandas as pd
 
 url = "https://www.finn.no/job/fulltime/search.html?abTestKey=control&extent=3947&location=1.20001.20061&occupation=0.23&sort=RELEVANCE"
 req = requests.get(url)
 soup = BeautifulSoup(req.content, 'html.parser')
-annonser = soup.find_all(class_="ads__unit__content")
-keywords = ['nyutdannet', '2022', 'utvikler', 'økonom', 'developer']
+job_ads = soup.find_all(class_="ads__unit__content")
+keywords = ['python', 'java', 'utvikler', 'data', 'developer', "maskin", "kunstig"]
 job_list = []
-for annonse in annonser:
+
+for job_ad in job_ads:
     for key in keywords:
-        contents = annonse.find_all(text=re.compile(key), class_="ads__unit__link")
-        keys = annonse.find_all(class_="ads__unit__content__keys")
+        contents = job_ad.find_all(text=re.compile(key), class_="ads__unit__link")
+        keys = job_ad.find_all(class_="ads__unit__content__keys")
         jobs = zip(keys, contents)
         for job in jobs:
-            ad = []
-            ad.append(job[0].contents[0].contents[0])
-            ad.append(job[1].contents[0])
-            ad.append(job[1].attrs['href'])
-            job_list.append([ad])
+            ad_content = []
+            ad_content.append(job[0].contents[0].contents[0])
+            ad_content.append(job[1].contents[0])
+            ad_content.append(job[1].attrs['href'])
+            job_list.append(ad_content)
 
-for item in job_list:
-    print(item)
+df = pd.DataFrame(job_list, columns=["Tittel", "Beskrivelse", "Url"])
+df.to_excel('job_list.xlsx')
